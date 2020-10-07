563cb72d50b5c3f0be1c1501b1d2f52032a0d600
shivranjanivyas1312 <vyasshivranjani@gmail.com>
2020-10-07 14:51:41 +0530

    initial commit

---
+++ src/_components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+import Search from './Search'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			
+			<Fragment>
+				<Search />
+			</Fragment>
+			
+		);
+	}
+}
